0ef748c3f753bb36835379a2f66f78f0252fc37b
akagrawal97 <akagrawaliit@gmail.com>
2022-05-01 22:47:36 +0530

    initializing the project

---
+++ App.js
@@ -0,0 +1,20 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import { NavigationContainer } from '@react-navigation/native';
+import React from 'react';
+
+const App = () => {
+  return(
+    <NavigationContainer>
+      
+    </NavigationContainer>
+  )
+}
+
+export default App;
